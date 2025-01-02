8d27631e415775b94bf389cf638445773667a061
mehdi <mehdibigi1383@gmail.com>
2025-01-02 15:39:13 +0330

    change

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <main />
-  </div>
+ <main>
+   <nuxt-layout>
+
+   </nuxt-layout>
+ </main>
 </template>
 <script setup lang="ts">
 import main from "~/components/main.vue";
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <section class="h-svh bg-main">
+    <header>
+    <h1>ah</h1>
+    </header>
+  </section>
+</template>
+<script setup lang="ts">
+</script>
