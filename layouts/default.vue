2772ecbeebad72d7b2f0fe1f4bce18e7bc5ceb1d
mehdi <mehdibigi1383@gmail.com>
2025-01-02 22:06:14 +0330

    more changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <header class="flex flex-col">
       <nav class="w-full flex justify-between px-10 mt-3">
         <v-btn variant="plain"> open </v-btn>
-        <div class="w-10 h-10 bg-neutral-400 rounded-full"></div>
+          <img  class="w-8 h-8" src="/images/user-icon.png" alt="">
       </nav>
     </header>
     <main class="basis-2/3 m-5">
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
 <template>
-  <div class="h-full w-full bg-neutral-300">
-    <video width="320" height="240">
-      <source src="/video/nikkha.mkv" />
-    </video>
+  <div class="h-full w-full bg-neutral-700 rounded-lg rounded-lgbg-neutral-300">
+
   </div>
 </template>
