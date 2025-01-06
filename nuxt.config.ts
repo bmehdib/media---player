604c452b32e2401ae2e9fcdadbf6b11f59316b03
mehdi <mehdibigi1383@gmail.com>
2025-01-07 00:51:39 +0330

    title change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'vuetify-nuxt-module']
+  modules: ['@nuxtjs/tailwindcss', 'vuetify-nuxt-module'],
+  app: {
+    head: {
+      title: 'video-Player'
+    }
+  }
 })
